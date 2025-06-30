672a2ec0461fc200b8b0c48db51d9fb85b4b1b96
AnamtaAhsan <anamtaahsan3@gmail.com>
2025-06-30 08:04:49 -0700

    scientific calculator

---
+++ app.js
@@ -0,0 +1,40 @@
+var input = document.getElementById('input')
+
+function Display(y){
+    if (y === '.' && input.value.includes('.')) {
+    return;
+  }
+    input.value += y
+}
+
+function ClearAll(){
+    input.value = ""
+}
+
+function applyFunc(func){
+  var val = parseFloat(input.value);
+    if (isNaN(val)) {
+      input.value = "Error";
+        return;
+    }
+
+    let result;
+
+      if (func === "sin") {
+        result = Math.sin(toRadians(val));
+      } else if (func === "cos") {
+        result = Math.cos(toRadians(val));
+      } else if (func === "tan") {
+        result = Math.tan(toRadians(val));
+      } else if (func === "sqrt") {
+        result = Math.sqrt(val);
+      } else if (func === "square") {
+        result = Math.pow(val, 2);
+      }
+
+      input.value = result.toFixed(12);
+    }
+
+function toRadians(degrees) {
+  return degrees * (Math.PI / 180);
+}
